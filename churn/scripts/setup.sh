947b8e84e35c535d2b50262113574f8eee2f1917
Pau Rodriguez <rodriguez.pau@gmail.com>
2020-08-10 01:23:02 +0200

    setup with venv , and EDA started in notebook

---
+++ churn/scripts/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+PROJECT_NAME=churn_example
+# setup environment
+setup_pyenv() {
+	ENVIRONMENT="$PROJECT_NAME_env"
+	pyenv install 3.7.2 -s
+	pyenv virtualenv 3.7.2 $ENVIRONMENT -f
+	pyenv local $ENVIRONMENT
+	pip install -r requirements.txt
+	pip install jupyter
+	python -m ipykernel install --user
+}
+
+
+
+# setup venv version
+setup_venv() {
+	set -e
+	mkdir -p .status
+	mkdir -p doc
+	mkdir -p data
+	mkdir -p src
+	mkdir -p notebooks
+	rm -rf ./venv/
+	python3 -m venv venv
+}
+
+
+# intall
+setup_packages() {
+	pip install -r requirements.txt
+}
+
+# setup ipython kernel
+jupyter_kernel() {
+	ipython kernel install --user --name="$PROJECT_NAME"
+	touch ./venv/.jupyter_set
+}
+
+
+setup_venv
+setup_packages
+jupyter_kernel
+touch .status/setup_done
+
+
